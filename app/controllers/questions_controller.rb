d0bc997d1f3080902b3961b530d8ad357d011fa5
Takeshi Shinohara <takeshigs@gmail.com>
2022-02-14 13:12:34 +0900

    added home page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,16 @@
 class QuestionsController < ApplicationController
+  def home; end
   def ask; end
 
-  def answer; end
+  def answer
   query = params[:question]
-  if query == "I am going to work"
-    
+  if query == 'I am going to work'
+    @answer = 'Great!'
+  elsif query.include?('?')
+    @answer = 'Silly question, get dressed and go to work!'
+  else
+    @answer = "I don't care, get dressed and go to work!"
+  end
+  @answer
+  end
 end
